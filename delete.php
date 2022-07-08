e24dea894c327a99c34979b9860fa5795951f69a
JavierAnz <javier.anzueto@galileo.edu>
2022-07-08 10:27:56 -0600

    curso de PHP y web

---
+++ delete.php
@@ -0,0 +1,28 @@
+<?php
+
+require "database.php";
+
+session_start();
+
+if (!isset($_SESSION["user"])) {
+  header("Location: login.php");
+  return;
+}
+
+$id = $_GET["id"];
+
+$statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement->execute([":id" => $id]);
+
+if ($statement->rowCount() == 0) {
+  http_response_code(404);
+  echo("HTTP 404 NOT FOUND");
+  return;
+}
+
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
+
+$_SESSION["flash"] = ["message" => "Contact {$contact['name']} deleted."];
+
+header("Location: home.php");
+return;
